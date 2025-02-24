63e3f38ff2ffc2bfe41a313d386aa550732c0e8a
José Victor Zeller <josevictorzg@gmail.com>
2025-02-24 12:22:44 -0300

    chore: add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule RabbitMQPoolEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [:ex_unit, :mix, :amqp, :poolboy],
+        list_unused_filters: true,
+        # we use the following opt to change the PLT path
+        # even though the opt is marked as deprecated, this is the doc-recommended way
+        # to do this
+        plt_file: {:no_warn, "_dialyzer/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -22,7 +31,12 @@ defmodule RabbitMQPoolEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:amqp, "~> 4.0"},
+
+      # Testing and Dev Tools
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
