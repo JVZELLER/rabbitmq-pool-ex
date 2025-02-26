8157cd62c7429fce6a0a165a5705b5bfc22ade6f
José Victor Zeller <josevictorzg@gmail.com>
2025-02-25 22:24:45 -0300

    refactor: add dedicated pool supervisor

--- lib/rabbitmq_pool_ex/application.ex
+++ lib/rabbitmq_pool_ex/application.ex
@@ -8,30 +8,13 @@ defmodule RabbitMQPoolEx.Application do
 
   @impl true
   def start(_type, args) do
-    children = poolboy_children(args)
+    children = [
+      {RabbitMQPoolEx.PoolSupervisor, args}
+    ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: RabbitMQPoolEx.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp poolboy_children(config) do
-    for pool_config <- Keyword.get(config, :connection_pools, []) do
-      {_registration_scope, pool_id} = Keyword.fetch!(pool_config, :name)
-
-      {channels_config, poolboy_config} =
-        Keyword.split(pool_config, ~w(channels reuse_channels?)a)
-
-      rabbitmq_config =
-        config |> Keyword.get(:rabbitmq_config, []) |> Keyword.merge(channels_config)
-
-      # We are using poolboy's pool as a fifo queue so we can distribute the
-      # load between workers
-      poolboy_config =
-        Keyword.merge(poolboy_config, worker_module: RabbitMQConnection, strategy: :fifo)
-
-      :poolboy.child_spec(pool_id, poolboy_config, rabbitmq_config)
-    end
   end
 end
---
+++ lib/rabbitmq_pool_ex/pool_supervisor.ex
@@ -0,0 +1,47 @@
+defmodule RabbitMQPoolEx.PoolSupervisor do
+  @moduledoc false
+
+  use Supervisor
+
+  alias RabbitMQPoolEx.Worker.RabbitMQConnection
+
+  @type config :: [rabbitmq_config: keyword(), connection_pools: list()]
+
+  @spec start_link(config()) :: Supervisor.on_start()
+  def start_link(config) do
+    Supervisor.start_link(__MODULE__, config)
+  end
+
+  @spec start_link(config(), atom()) :: Supervisor.on_start()
+  def start_link(config, name) do
+    Supervisor.start_link(__MODULE__, config, name: name)
+  end
+
+  @impl true
+  def init(config) do
+    children = poolboy_children(config)
+
+    opts = [strategy: :one_for_one]
+
+    Supervisor.init(children, opts)
+  end
+
+  defp poolboy_children(config) do
+    for pool_config <- Keyword.get(config, :connection_pools, []) do
+      {_registration_scope, pool_id} = Keyword.fetch!(pool_config, :name)
+
+      {channels_config, poolboy_config} =
+        Keyword.split(pool_config, ~w(channels reuse_channels?)a)
+
+      rabbitmq_config =
+        config |> Keyword.get(:rabbitmq_config, []) |> Keyword.merge(channels_config)
+
+      # We are using poolboy's pool as a fifo queue so we can distribute the
+      # load between workers
+      poolboy_config =
+        Keyword.merge(poolboy_config, worker_module: RabbitMQConnection, strategy: :fifo)
+
+      :poolboy.child_spec(pool_id, poolboy_config, rabbitmq_config)
+    end
+  end
+end
